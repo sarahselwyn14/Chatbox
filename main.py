8695f8f1336a95d7a439cee405dbe0894521e225
sarahselwyn025 <26705827-sarahselwyn025@users.noreply.replit.com>
2023-12-23 04:35:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+print("Welcome to iChat Chatbox!")
+user_name = input("Before we get started, what is your name?")
+print(f'Hello {user_name}! ')
+user_age = input("What is your age?")
+print("That's great!")
+
+
+def chat_functions():
+  print("-- 1. Placeholder --")
+  print("-- 2. Placeholder --")
+  print("-- 3. Placeholder --")
+  print("-- 4. Placeholder --")
+  print("-- 5. Exit Chat --")
+  
+
+chat_functions()
+user_choice = input("What would you like to do?(input a number 1-5)")
+
+if user_choice == "1":
+   print("Placeholder 1 !")
+if user_choice == "2":
+   print("Placeholder 2 !")
+if user_choice == "3":
+   print("Placeholder 3 !")
+if user_choice == "4":
+   print("Placeholder 4 !")
+if user_choice == "5":
+   print("Exiting Chatbox...")
+   quit()     
+
+  
