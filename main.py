f280db5edffbba879413db01e523cd0a7053a0fb
sarahselwyn14 <154290856+sarahselwyn14@users.noreply.github.com>
2024-01-09 23:55:00 +0000

    Changed the chat_functions, placeholders and exit message

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ user_name = input("Before we get started, what is your name?")
 print(f'Hello {user_name}! ')
 user_age = input("What is your age?")
 print("That's great!")
+print(f'How may I assist you {user_name}?')
 
 
 def chat_functions():
-  print("-- 1. Placeholder --")
-  print("-- 2. Placeholder --")
-  print("-- 3. Placeholder --")
-  print("-- 4. Placeholder --")
+  print("-- 1. Placeholder (Write a passage) --")
+  print("-- 2. Placeholder (Answer questions) --")
+  print("-- 3. Placeholder (Describe in detail) --")
+  print("-- 4. Placeholder (Have a Conversation) --")
   print("-- 5. Exit Chat --")
   
 
@@ -25,7 +26,10 @@ if user_choice == "3":
 if user_choice == "4":
    print("Placeholder 4 !")
 if user_choice == "5":
-   print("Exiting Chatbox...")
-   quit()     
+  print("Thank you for using iChat Chatbox!")
+  print("Exiting Chatbox...")
+  quit()     
 
   
+# The only source that I used as reference for this assignment 
+# was my previous coding projects done in Wave 1 for Software Development.
